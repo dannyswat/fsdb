4db7b40d3238d969f166dc169bb79458b689f886
Dannys Wat <dhlwat@live.com>
2025-06-01 00:17:37 +0800

    Add some testing code in main

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/dannyswat/fsdb/datatype"
+	"github.com/dannyswat/fsdb/fsdb"
+)
+
+func main() {
+	db, err := fsdb.NewDatabase("mydb")
+	if err != nil {
+		panic(err)
+	}
+	err = db.CreateCollection(fsdb.CollectionSchema{
+		Name: "users",
+		Columns: []fsdb.ColumnDefinition{
+			{FieldName: "id", DataType: datatype.Integer},
+			{FieldName: "name", DataType: datatype.String},
+		},
+		Indexes: []fsdb.IndexDefinition{
+			{
+				Name:        "pk_users",
+				IsClustered: true,
+				Keys:        []fsdb.IndexField{{Name: "id"}},
+				PageSize:    10,
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	collection, err := db.GetCollection("users")
+	if err != nil {
+		panic(err)
+	}
+	collection.Insert(map[string]any{"id": 1, "name": "Alice"})
+	collection.Insert(map[string]any{"id": 2, "name": "Bob"})
+	collection.Insert(map[string]any{"id": 3, "name": "Charlie"})
+	collection.Insert(map[string]any{"id": 4, "name": "Diana"})
+	fmt.Println("Inserted 4 users")
 }
